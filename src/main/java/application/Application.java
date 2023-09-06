4302b6c214487b981f81d76dcfde28dc97759357
Anna <anna.chymshyt@gmail.com>
2023-09-06 17:41:56 +0300

    Add Application

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,11 @@
+package application;
+
+import application.utils.AppStarter;
+
+public class Application {
+
+    public static void main(String[] args) {
+        AppStarter.startApp();
+    }
+
+}

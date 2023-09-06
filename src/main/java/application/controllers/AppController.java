9b91eabe991984d23a135eb35729649facb913ee
Anna <anna.chymshyt@gmail.com>
2023-09-06 17:41:13 +0300

    Add AppController

---
+++ src/main/java/application/controllers/AppController.java
@@ -0,0 +1,32 @@
+package application.controllers;
+
+import application.services.AppService;
+import application.utils.Constants;
+import application.views.AppView;
+
+public class AppController {
+    AppView view;
+    AppService service;
+
+    public AppController(AppView view, AppService service) {
+        this.view = view;
+        this.service = service;
+    }
+
+    public void runApp() {
+        filterChoice(view.chooseOption());
+    }
+
+    private void filterChoice(int choice) {
+        switch (choice) {
+            case 1 -> service.createUser();
+            case 2 -> service.readUsers();
+            case 3 -> service.updateUser();
+            case 4 -> service.deleteUser();
+            case 0 -> view.getOutput(choice, Constants.APP_CLOSE_MSG);
+            default -> service.getNoSuchOption(choice);
+        }
+    }
+
+
+}
